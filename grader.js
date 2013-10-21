a2c0722905d85be84b9103ff44a6a04a7fd45912
Rodrigo Sanabria <r.sanabriadc@gmail.com>
2013-10-21 03:31:17 +0000

    grader listo

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 var URL_DEFAULT = "http://protected-reef-3662.herokuapp.com/";
@@ -39,6 +40,9 @@ var assertFileExists = function(infile) {
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+var cheerioUrlResult= function(url_result){
+  return cheerio.load(url_result);
 };
 
 var loadChecks = function(checksfile) {
@@ -64,13 +68,21 @@ var clone = function(fn) {
 
 var getUrl = function(url_result, checksfile) {
 $ = cheerioUrlResult(url_result);
+   var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+  var present = $(checks[ii]).length > 0;
+  out[checks[ii]] = present;
+    }
+    return out;
+};
 
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.option('-u, --url <url_result>', 'Comand line defined URL', URL_DEFAULT)
-	.parse(process.argv);
+	.parse(process.argv)
 
     if(program.url) {
 	console.log(' - url' + "argument");
@@ -83,14 +95,15 @@ if(require.main == module) {
 //	    var checkJson = checkHtmlFile(program.file, program.checks);
 //	    var outJson = JSON.stringify(checkJson, null, 4);
 	    console.log(outJson);
-});
+})
 
     } else {
 	var checkJson = checkHtmlFile(program.file, program.checks);
 	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson); }
-
-} else {
+	console.log(outJson); 
+}
+} 
+else {
 	exports.checkHtmlFile = checkHtmlFile;
 
 }
